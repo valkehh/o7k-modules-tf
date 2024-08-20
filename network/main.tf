03154c8b7f99511a8ae7d8a364b668b683929967
valkeh <albin_berg@yahoo.com>
2024-08-20 02:43:37 +0200

    added dns server

--- network/main.tf
+++ network/main.tf
@@ -11,6 +11,7 @@ resource "openstack_networking_subnet_v2" "net1_subnet1" {
   name       = format("%s-network1-subnet1", var.network_prefix)
   network_id = openstack_networking_network_v2.net1.id
   cidr       = var.subet_cidr
+  dns_nameservers = ["8.8.8.8","8.8.4.4"]
   allocation_pool {
     start = var.subnet_allocation_pool.start
     end   = var.subnet_allocation_pool.end
